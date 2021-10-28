bf6bca4f4105c0898847d80987afad06edc201ac
Abhinav Sharma <abhi18av@gmail.com>
2021-10-28 17:19:32 +0000

    inital commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bionitio-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/test/clojure/net/clojars/abhi18av/bionitio_clojure_test.clj
@@ -0,0 +1,7 @@
+(ns net.clojars.abhi18av.bionitio-clojure-test
+  (:require [clojure.test :refer :all]
+            [net.clojars.abhi18av.bionitio-clojure :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
